494fda9953a0d3658ce32734ff083e19d4ce5798
adi12 <adi1231234@gmail.com>
2019-10-12 17:47:41 +0300

    Add RaFor option use it in MainTlunot component

--- configuraion.js
+++ configuraion.js
@@ -1,30 +1,73 @@
 ﻿const data = {
-    Shelot: [{"shela": "חבלה", type: "check", Tlon: ["כאבי ראש", "כאבי בטן", "כאבי גרון"], Pre: ""},
-        {shela: "סחרחורת", type: "check", Tlon: ["כאבי ראש", "כאבי בטן", "כאבי גרון"], Pre: "0"},
-        {shela: "בחילות", type: "check", Tlon: ["כאבי ראש", "כאבי בטן"], Pre: "0"},
-        {shela: "הקאות", type: "amount", Tlon: ["כאבי ראש", "כאבי בטן"], Pre: "0"},
-        {shela: "הקאה דמית ", type: "amount", Tlon: ["כאבי ראש", "כאבי בטן"], Pre: "הקאות"},
-        {shela: "טשטוש בראייה", type: "check", Tlon: ["כאבי ראש"], Pre: "0"},
-        {shela: "נזלת", type: "check", Tlon: ["כאבי ראש", "כאבי גרון"], Pre: "0"},
-        {shela: "התגברות הכאב בהטיית הראש קדימה", type: "check", Tlon: ["כאבי ראש"], Pre: "0"},
-        {shela: "עליית חום", type: "num", Tlon: ["כאבי ראש", "כאבי בטן", "כאבי גרון"], Pre: "0"},
-        //כאבי בטן
-        {shela: "שלשולים", type: "amount", Tlon: ["כאבי בטן"], Pre: "0"},
-        {shela: "שלשול דמי", type: "check", Tlon: ["כאבי בטן"], Pre: "שלשולים"},
-        {shela: "מאכל חריג", type: "check", Tlon: ["כאבי בטן"], Pre: "0"},
-        {shela: "מקרים דומים בסביבה", type: "check", Tlon: ["כאבי בטן"], Pre: "0"},
-        //כאבי גרון
-        {shela: "שיעול", type: "check", Tlon: ["כאבי גרון"], Pre: "0"},
-        {shela: "קושי בבליעה", type: "check", Tlon: ["כאבי גרון"], Pre: "0"},
-        {shela: "קושי בדיבור", type: "check", Tlon: ["כאבי גרון"], Pre: "0"},
-        {shela: "היסטוריה של דלקות גרון", type: "check", Tlon: ["כאבי גרון"], Pre: "0"},
-        {shela: "בקרה", type: "amount", Tlon: ["היי"]}]
+  Shelot: [
+    {
+      shela: "חבלה",
+      type: "check",
+      Tlon: ["כאבי ראש", "כאבי בטן", "כאבי גרון"],
+      Pre: ""
+    },
+    {
+      shela: "סחרחורת",
+      type: "check",
+      Tlon: ["כאבי ראש", "כאבי בטן", "כאבי גרון"],
+      Pre: "0"
+    },
+    {
+      shela: "בחילות",
+      type: "check",
+      Tlon: ["כאבי ראש", "כאבי בטן"],
+      Pre: "0"
+    },
+    {
+      shela: "הקאות",
+      type: "amount",
+      Tlon: ["כאבי ראש", "כאבי בטן"],
+      Pre: "0"
+    },
+    {
+      shela: "הקאה דמית ",
+      type: "amount",
+      Tlon: ["כאבי ראש", "כאבי בטן"],
+      Pre: "הקאות"
+    },
+    { shela: "טשטוש בראייה", type: "check", Tlon: ["כאבי ראש"], Pre: "0" },
+    { shela: "נזלת", type: "check", Tlon: ["כאבי ראש", "כאבי גרון"], Pre: "0" },
+    {
+      shela: "התגברות הכאב בהטיית הראש קדימה",
+      type: "check",
+      Tlon: ["כאבי ראש"],
+      Pre: "0"
+    },
+    {
+      shela: "עליית חום",
+      type: "num",
+      Tlon: ["כאבי ראש", "כאבי בטן", "כאבי גרון"],
+      Pre: "0"
+    },
+    //כאבי בטן
+    { shela: "שלשולים", type: "amount", Tlon: ["כאבי בטן"], Pre: "0" },
+    { shela: "שלשול דמי", type: "check", Tlon: ["כאבי בטן"], Pre: "שלשולים" },
+    { shela: "מאכל חריג", type: "check", Tlon: ["כאבי בטן"], Pre: "0" },
+    {
+      shela: "מקרים דומים בסביבה",
+      type: "check",
+      Tlon: ["כאבי בטן"],
+      Pre: "0"
+    },
+    //כאבי גרון
+    { shela: "שיעול", type: "check", Tlon: ["כאבי גרון"], Pre: "0" },
+    { shela: "קושי בבליעה", type: "check", Tlon: ["כאבי גרון"], Pre: "0" },
+    { shela: "קושי בדיבור", type: "check", Tlon: ["כאבי גרון"], Pre: "0" },
+    {
+      shela: "היסטוריה של דלקות גרון",
+      type: "check",
+      Tlon: ["כאבי גרון"],
+      Pre: "0"
+    },
+    { shela: "בקרה", type: "amount", Tlon: ["היי"] }
+  ]
 };
 
-
 const TheProblems = {
-    Tlonot: [{Tlon: "כאבי ראש"},
-        {Tlon: "כאבי בטן"},
-        {Tlon: "כאבי גרון"}
-    ]
+  Tlonot: [{ Tlon: "כאבי ראש" }, { Tlon: "כאבי בטן" }, { Tlon: "כאבי גרון" }]
 };
